6060844f968a3e5dbebab83ba38e018a671a5a49
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-05-20 22:34:40 +0300

    Actually add the DragThumb class to the repo............

---
+++ issb/DragThumb.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+
+namespace issb {
+    public class DragThumb : Thumb {
+        private void DraggableThumb_DragDelta(object sender, DragDeltaEventArgs eventArgs)
+        {
+            Control item = DataContext as Control;
+
+            if(item != null) {
+                double leftOffset = Canvas.GetLeft(item);
+                double topOffset = Canvas.GetTop(item);
+
+                Canvas.SetLeft(item, leftOffset + eventArgs.HorizontalChange);
+                Canvas.SetTop(item, topOffset + eventArgs.VerticalChange);
+            }
+        }
+
+        public DragThumb()
+        {
+            DragDelta += new DragDeltaEventHandler(DraggableThumb_DragDelta);
+        }
+    }
+}
