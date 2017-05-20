1c76994cd157ca27b2aa04164bb89122737443ea
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-05-21 00:47:32 +0300

    Implement C.Control resize (no a/r preservation)

---
+++ issb/ResizeThumb.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+
+namespace issb
+{
+    class ResizeThumb : Thumb
+    {
+        private void ResizeThumb_DragDelta(object sender, DragDeltaEventArgs eventArgs)
+        {
+            Control item = DataContext as Control;
+
+            if(item != null) {
+                double dVertical, dHorizontal;
+
+                /*
+                TODO: preserve aspect ratio
+                */
+
+                switch(VerticalAlignment) {
+                    case System.Windows.VerticalAlignment.Top:
+                        dVertical = Math.Min(
+                            eventArgs.VerticalChange,
+                            item.ActualHeight - item.MinHeight
+                            );
+                        Canvas.SetTop(item, Canvas.GetTop(item) + dVertical);
+                        item.Height -= dVertical;
+                        break;
+                    case System.Windows.VerticalAlignment.Bottom:
+                        dVertical = Math.Min(
+                            -eventArgs.VerticalChange,
+                            item.ActualHeight - item.MinHeight
+                            );
+                        item.Height -= dVertical;
+                        break;
+                    default:
+                        break;
+                }
+
+                switch(HorizontalAlignment) {
+                    case System.Windows.HorizontalAlignment.Left:
+                        dHorizontal = Math.Min(
+                            eventArgs.HorizontalChange,
+                            item.ActualWidth - item.MinWidth
+                            );
+                        Canvas.SetLeft(item, Canvas.GetLeft(item) + dHorizontal);
+                        item.Width -= dHorizontal;
+                        break;
+                    case System.Windows.HorizontalAlignment.Right:
+                        dHorizontal = Math.Min(
+                            -eventArgs.HorizontalChange,
+                            item.ActualWidth - item.MinWidth
+                            );
+                        item.Width -= dHorizontal;
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            eventArgs.Handled = true;
+        }
+
+        public ResizeThumb()
+        {
+            DragDelta += new DragDeltaEventHandler(ResizeThumb_DragDelta);
+        }
+    }
+}
