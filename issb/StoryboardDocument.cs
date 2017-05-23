f1eb928fb1d3a378e333fb07aeb8e845b1c479da
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-05-23 23:50:40 +0300

    Add a stub from StoryboardDocument

---
+++ issb/StoryboardDocument.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace issb
+{
+    class StoryboardDocument
+    {
+        BackgroundTemplate Template;
+
+        List<BitmapImage> FrameBackgrounds;
+
+        List<Rect> StoryboardItemRects;
+        List<BitmapImage> StoryboardItemBitmapImages;
+
+        StoryboardDocument() { }
+
+        public static StoryboardDocument LoadFromCanvas(StoryboardCanvas storyboardCanvas)
+        {
+            StoryboardDocument newDocument = new StoryboardDocument();
+
+            //
+
+            return newDocument;
+        }
+
+        public void UnloadOntoCanvas(StoryboardCanvas storyboardCanvas)
+        {
+
+        }
+
+        public static StoryboardDocument LoadFromFile(FileStream fileStream)
+        {
+            StoryboardDocument newDocument = new StoryboardDocument();
+
+            //
+
+            return newDocument;
+        }
+
+        public void SaveToFile(FileStream fileStream)
+        {
+
+        }
+    }
+}
