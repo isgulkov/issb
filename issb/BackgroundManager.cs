711ae9742eb11158b454baaa67d4e0cdb978ba4e
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-05-24 23:42:01 +0300

    Remove and sort usings in BackgroundManager.cs

--- issb/BackgroundManager.cs
+++ issb/BackgroundManager.cs
@@ -1,11 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
-using System.Collections.Generic;
 using System.Windows.Controls;
-using System.Windows.Shapes;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
 
 namespace issb
 {
