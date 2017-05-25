56681724ef9c6d0fc09c5f9772ee36c0f05506d5
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-05-25 03:37:16 +0300

    Remove usings BackgroundTemplate.cs

--- issb/BackgroundTemplate.cs
+++ issb/BackgroundTemplate.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
-using System.Xml;
 using System.Windows;
+using System.Xml;
 
 namespace issb
 {
